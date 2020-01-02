0dea90ddb047d8e12d825253554c270e94e3abc3
vict <przemek.maranowski@gmail.com>
2020-01-02 12:08:06 +0100

    Updates

--- precache-manifest.e24e87385e7913f77e1719460228b1a3.js
+++ precache-manifest.e24e87385e7913f77e1719460228b1a3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4a977f52cf740b49dc2d1648a68ce1d8",
+    "revision": "8f453fd82d8656e6d07d6f4528e00e2f",
     "url": "/spy/index.html"
   },
   {
-    "revision": "40986056395d2944d390",
+    "revision": "7b2bec748aec1e631140",
     "url": "/spy/static/css/main.1b299fad.chunk.css"
   },
   {
-    "revision": "a4175aee95d0fa13cb18",
-    "url": "/spy/static/js/2.54156ead.chunk.js"
+    "revision": "b968550bc25f37b47d34",
+    "url": "/spy/static/js/2.69e2ff28.chunk.js"
   },
   {
     "revision": "b1cb59503a3f86efe7e8ca43051433df",
-    "url": "/spy/static/js/2.54156ead.chunk.js.LICENSE"
+    "url": "/spy/static/js/2.69e2ff28.chunk.js.LICENSE"
   },
   {
-    "revision": "40986056395d2944d390",
-    "url": "/spy/static/js/main.5d95d2ba.chunk.js"
+    "revision": "7b2bec748aec1e631140",
+    "url": "/spy/static/js/main.5be9759b.chunk.js"
   },
   {
     "revision": "cab372b76c5314159cea",
