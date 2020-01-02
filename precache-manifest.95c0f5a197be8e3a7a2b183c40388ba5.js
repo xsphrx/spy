85f5e7998f76e7aa158cd4372995ae8d8b1af053
vict <przemek.maranowski@gmail.com>
2020-01-02 01:31:50 +0100

    Updates

---
+++ precache-manifest.95c0f5a197be8e3a7a2b183c40388ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703acacd004fe0683f7c312a7ae608b4",
+    "url": "/spy/index.html"
+  },
+  {
+    "revision": "23f871587ba74135d8eb",
+    "url": "/spy/static/css/main.1b299fad.chunk.css"
+  },
+  {
+    "revision": "d6b04d512509cb9e9375",
+    "url": "/spy/static/js/2.f320211d.chunk.js"
+  },
+  {
+    "revision": "b1cb59503a3f86efe7e8ca43051433df",
+    "url": "/spy/static/js/2.f320211d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23f871587ba74135d8eb",
+    "url": "/spy/static/js/main.d365c3b8.chunk.js"
+  },
+  {
+    "revision": "cab372b76c5314159cea",
+    "url": "/spy/static/js/runtime-main.f452f00c.js"
+  }
+]);
